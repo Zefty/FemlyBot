7e1e934135ad6fbbd55f165a3cf96c4fef3cba3a
zefty <jaime.wu011@gmail.com>
2021-08-15 12:39:35 +1200

    create express server

--- bot.ts
+++ bot.ts
@@ -1,14 +1,17 @@
-import Discord, { Interaction, GuildMember, Snowflake } from 'discord.js';
-import { AudioPlayerStatus, AudioResource, entersState, joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
+import express from 'express'
+import bodyParser from 'body-parser'
+import Discord, { GuildMember, Snowflake } from 'discord.js';
+import { joinVoiceChannel } from '@discordjs/voice';
 import { Track } from './music/track';
 import { MusicSubscription } from './music/subscription';
-const token = process.env.DISCORD_TOKEN;
 
+const PORT = process.env.PORT || 5001;
+const TOKEN = process.env.DISCORD_TOKEN;
+const app = express(); app.use(bodyParser.json());
 const client = new Discord.Client({ intents: ['GUILD_VOICE_STATES', 'GUILD_MESSAGES', 'GUILDS'] });
+const subscriptions = new Map<Snowflake, MusicSubscription>();
 
 client.on('ready', () => console.log('Ready!'));
-
-const subscriptions = new Map<Snowflake, MusicSubscription>();
 
 client.on('messageCreate', async (message: Discord.Message) => {
 	if (!message.guild) return;
@@ -17,6 +20,7 @@ client.on('messageCreate', async (message: Discord.Message) => {
 
 	if (message.content.toLowerCase().includes('family') || message.content.toLowerCase() === 'femily') { 
 		if (!message.guildId) return;
+		console.log(`Playing on Guild: ${message.guildId}`)
 		let subscription = subscriptions.get(message.guildId);
 		if (!subscription) {
 			if (message.member instanceof GuildMember && message.member.voice.channel) {
@@ -50,7 +54,7 @@ client.on('messageCreate', async (message: Discord.Message) => {
 					if (!subscription){
 						return
 					}
-					console.log("Start Song!");
+					console.log(`Playing: https://www.youtube.com/watch?v=RgKAFK5djSk`);
 					subscription.enqueue(track);
 				},
 				onFinish() {
@@ -60,6 +64,7 @@ client.on('messageCreate', async (message: Discord.Message) => {
 				},
 			});
 			if (subscription) {
+				await message.reply("♫ I don't have friends, I got family 👪 ♫");
 				subscription.enqueue(track);
 			}
 		} catch (error) {
@@ -70,4 +75,35 @@ client.on('messageCreate', async (message: Discord.Message) => {
 
 client.on('error', console.warn);
 
-void client.login(token);
+void client.login(TOKEN);
+
+app.get('/', async (req, res) => {
+    res.send("FemilyBot - I don't have friends, I got family 👪")
+})
+
+app.post('/play', async (req, res) => {
+	console.log(req.body)
+	let subscription = subscriptions.get(req.body.guildId);
+	if (!subscription) {
+		res.send("Not playing in this server!")		
+	} else {
+		subscription.queueLock = false;
+		const track = await Track.from(req.body.link, {
+				onStart() {
+					console.log(`Playing: ${req.body.link}`);
+				},
+				onFinish() {
+				},
+				onError(error) {
+					console.warn(error);
+				},
+		});
+		subscription.enqueue(track);
+		res.send(`Queuing: ${req.body.link}`)
+	}
+
+})
+
+app.listen(PORT, () => {
+    console.log(`FemilyBot is running on port ${ PORT }`);
+});
